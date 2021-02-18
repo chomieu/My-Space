940463f27b784d74ff41a334a7732f2b29fc26ac
chomieu <chomieu@gmail.com>
2021-02-17 19:32:43 -0800

    Edited Header's p tag value

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="jumbotron text-center m-0">
       <h1>Employee Directory</h1>
-      <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+      <p className="lead">Click on "Name" to toggle sort or use the search box to narrow your results.</p>
     </div>
   )
 }
