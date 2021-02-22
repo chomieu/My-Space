9f32cfe0aaa165d15d07cd3c1f8b64b4d46796fd
chomieu <chomieu@gmail.com>
2021-02-22 04:09:41 -0800

    Edited carousel

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -2,42 +2,31 @@ import React from "react"
 import Font from "react-font"
 import { Carousel } from "react-bootstrap"
 
-export default function Header(props) {
+export default function Header() {
   return (
     <Carousel className="d-flex align-self-center" id="slideShow">
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/mteTq4g.jpg"
+          src="https://i.imgur.com/MJ5Rf8k.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla</p>
+          <Font family="Concert One">
+            <h5 className="text-border mb-0 mt-2">Meets</h5>
+          </Font>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/mteTq4g.jpg"
+          src="https://i.imgur.com/lLu2vqh.png"
           alt="Second slide"
         />
-
         <Carousel.Caption>
-        <h5>First slide label</h5>
-          <p>Nulla</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.imgur.com/mteTq4g.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-        <h5>First slide label</h5>
-          <p>Nulla</p>
+          <Font family="Concert One">
+            <h5 className="text-border mb-0 mt-2">Voicely</h5>
+          </Font>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
