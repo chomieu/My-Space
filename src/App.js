641e74189836b26758ed01767734953325efe45d
chomieu <chomieu@gmail.com>
2021-02-24 17:07:34 -0800

    Switched from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Landing from "./pages/Landing"
 import Projects from "./pages/Projects";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <main>
-        <Switch>
-          <Route exact path={["/", "/React-Portfolio"]}>
-            <Landing />
-          </Route>
-          <Route exact path={["/About", "/React-Portfolio/About"]}>
-            <About />
-          </Route>
-          <Route exact path={["/Projects", "/React-Portfolio/Projects"]}>
-            <Projects />
-          </Route>
-          <Route exact path={["/Contact", "/React-Portfolio/Contact"]}>
-            <Contact />
-          </Route>
-        </Switch>
-      </main>
+      <Switch>
+        <Route exact path={["/", "/React-Portfolio"]}>
+          <Landing />
+        </Route>
+        <Route exact path={["/About", "/React-Portfolio/About"]}>
+          <About />
+        </Route>
+        <Route exact path={["/Projects", "/React-Portfolio/Projects"]}>
+          <Projects />
+        </Route>
+        <Route exact path={["/Contact", "/React-Portfolio/Contact"]}>
+          <Contact />
+        </Route>
+      </Switch>
     </Router>
   );
 }
