f94104724428024a587d034f414e8ec3f377549e
chomieu <chomieu@gmail.com>
2021-02-24 20:15:47 -0800

    Edited project page

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,49 +1,60 @@
 import React from "react"
 import Font from "react-font"
-import { Carousel } from "react-bootstrap"
+import { Carousel, Button } from "react-bootstrap"
 
 export default function Header() {
   return (
-    <Carousel className="d-flex align-self-center" id="slideShow">
+    <Carousel className="d-flex align-self-center p-1 pb-0" id="slideShow">
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/t3cMxZ5.jpg"
+          src="https://i.imgur.com/PV0WiqE.jpg"
           alt="Intro slide"
         />
-        <Carousel.Caption>
+        <div style={{ opacity: 1 }}>
           <Font family="Concert One">
-            <p className="my-1 py-1">My Recent Work</p>
+            <p className="mb-3 text-center">My Recent Work</p>
             <h4>This slideshow contains</h4>
-            <h4> projects I've worked on recently.</h4>
-            <p className="my-1 py-1">For more examples</p>
-            <h4>Email: chomieu@gmail.com</h4>
+            <h4>projects I've worked on recently.</h4>
+            <h4>Hover on them for more details!</h4>
           </Font>
-        </Carousel.Caption>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/MJ5Rf8k.png"
+          src="https://i.imgur.com/wHayz1A.jpg"
           alt="First slide"
         />
-        <Carousel.Caption>
+        <div>
           <Font family="Concert One">
-            <h5 className="text-border mb-0 mt-2">Meets</h5>
+            <h3 className="text-border">Meets</h3>
+            <h4>Next generation social planning</h4>
+            <h4>app with A.I. integration.</h4>
+            <div className="d-flex pb-3 pt-2 pt-sm-3">
+              <Button variant="light" size="sm mr-3">Repository</Button>
+              <Button variant="light" size="sm">Live Site</Button>
+            </div>
           </Font>
-        </Carousel.Caption>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://i.imgur.com/lLu2vqh.png"
+          src="https://i.imgur.com/IN70QbQ.jpg"
           alt="Second slide"
         />
-        <Carousel.Caption>
+        <div>
           <Font family="Concert One">
-            <h5 className="text-border mb-0 mt-2">Voicely</h5>
+            <h3 className="text-border">Voicely</h3>
+            <h4>Notepad with Speech-to-Text technology</h4>
+            <h4>and ability to parse text to an emoji.</h4>
+            <div className="d-flex pb-3 pt-2 pt-sm-3">
+              <Button variant="light" size="sm mr-3">Repository</Button>
+              <Button variant="light" size="sm">Live Site</Button>
+            </div>
           </Font>
-        </Carousel.Caption>
+        </div>
       </Carousel.Item>
     </Carousel>
   )
