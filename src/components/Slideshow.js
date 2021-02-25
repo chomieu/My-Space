19a3e61e2e0464468c11575b1e9580b62c097670
chomieu <chomieu@gmail.com>
2021-02-24 20:22:22 -0800

    Adjusted buttons

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -31,9 +31,9 @@ export default function Header() {
             <h3 className="text-border">Meets</h3>
             <h4>Next generation social planning</h4>
             <h4>app with A.I. integration.</h4>
-            <div className="d-flex pb-3 pt-2 pt-sm-3">
-              <Button variant="light" size="sm mr-3">Repository</Button>
-              <Button variant="light" size="sm">Live Site</Button>
+            <div className="d-flex pb-3 pt-sm-3">
+              <Button variant="light" className="mr-3">Repository</Button>
+              <Button variant="light">Live Site</Button>
             </div>
           </Font>
         </div>
@@ -49,9 +49,9 @@ export default function Header() {
             <h3 className="text-border">Voicely</h3>
             <h4>Notepad with Speech-to-Text technology</h4>
             <h4>and ability to parse text to an emoji.</h4>
-            <div className="d-flex pb-3 pt-2 pt-sm-3">
-              <Button variant="light" size="sm mr-3">Repository</Button>
-              <Button variant="light" size="sm">Live Site</Button>
+            <div className="d-flex pb-3 pt-sm-3">
+              <Button variant="light" className="mr-3">Repository</Button>
+              <Button variant="light">Live Site</Button>
             </div>
           </Font>
         </div>
